0dd5bb33683dc83c2b3fa536ff194fe9d3f245e4
yisroelgugenheim <126514996+yisroelgugenheim@users.noreply.github.com>
2023-11-07 14:29:40 -0600

    added useEffect to load all blog posts and used axios to connect to api

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
 import { Modal } from "./Modal";
+import axios from "axios";
 
 export function Content() {
+  const [posts, setPosts] = useState([]);
+
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data);
+      setIsPostsShowVisible(response.data);
+      setPosts(response.data);
+    });
+  };
+
   const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
 
   const handleShowPost = () => {
@@ -14,50 +25,11 @@ export function Content() {
     setIsPostsShowVisible(false);
   };
 
-  let posts = [
-    {
-      id: 1,
-      title: "Blog Entry #1",
-      body: "Blah blah blah yappity yap blog blog blog",
-      image: "https://previews.123rf.com/images/ewe/ewe0809/ewe080900216/3554960-blog.jpg",
-    },
-    {
-      id: 2,
-      title: "Blog Entry #2",
-      body: "Blah blah blah yadda yadda yadda blog blog blog",
-      image: "https://previews.123rf.com/images/60dudek/60dudek1901/60dudek190100006/115910734-word-blog.jpg",
-    },
-    {
-      id: 3,
-      title: "Blog Entry #3",
-      body: "Bloggity blog blog",
-      image:
-        "https://previews.123rf.com/images/dogfella/dogfella1601/dogfella160100013/50478147-stack-of-small-wood-lettes-as-blog-word.jpg",
-    },
-    {
-      id: 4,
-      title: "Blog Entry #4",
-      body: "Blah blah blah yappity yap blog blog blog",
-      image: "https://previews.123rf.com/images/ewe/ewe0809/ewe080900216/3554960-blog.jpg",
-    },
-    {
-      id: 5,
-      title: "Blog Entry #5",
-      body: "Blah blah blah yadda yadda yadda blog blog blog",
-      image: "https://previews.123rf.com/images/60dudek/60dudek1901/60dudek190100006/115910734-word-blog.jpg",
-    },
-    {
-      id: 6,
-      title: "Blog Entry #6",
-      body: "Bloggity blog blog",
-      image:
-        "https://previews.123rf.com/images/dogfella/dogfella1601/dogfella160100013/50478147-stack-of-small-wood-lettes-as-blog-word.jpg",
-    },
-  ];
+  useEffect(handleIndexPosts, []);
 
   return (
     <div id="contentDiv">
-      <PostsIndex posts={posts} onShowPost={handleShowPost}/>
+      <PostsIndex posts={posts} onShowPost={handleShowPost} />
       <PostsNew />
       <Modal show={isPostsShowVisible} onClose={handleClose}>
         <p>test</p>
